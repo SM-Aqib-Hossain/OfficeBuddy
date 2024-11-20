f44ea5dfa5ea32f37ca9042d36255366d4685f9b
aqib.hossain <aqib.hossain@DM-50005>
2024-11-20 18:57:56 +0600

    good

--- DataAccessLayer/Entities/CoinToss.cs
+++ DataAccessLayer/Entities/CoinToss.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entities
+{
+    internal class CoinToss
+    {
+        public int Id { get; set; }
+        public string playerId { get; set; }
+        public string playerChoice { get; set; }
+        public string computerResult { get; set; }
+        public bool playerWin { get; set; }
+    }
+}
